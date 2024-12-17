e4a1ad37eb422ea01ab8d4cfe36c2cc42bba1f4b
Allen Peng <allenpeng@wirelessprv-10-193-179-43.near.illinois.edu>
2024-12-16 18:28:49 -0600

    added line detector

--- src/line_detect.py
+++ src/line_detect.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
+import os
 
-def greyfill():
-    img = cv2.imread('line_sample.jpg', cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+def greyfill(img):
+    #img = cv2.imread('output_clean_p1.jpg', cv2.IMREAD_GRAYSCALE)
+    #img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
     img = img/255
 
-    img = img[0:800, :]
-    #cv2.imshow("img", img)
-    #cv2.waitKey(0)
-
+    #img = img[0:800, :]
     threshold = 0.5
     #create binarized image to enhance lines
     for i in range(0, img.shape[0]):
@@ -20,18 +18,29 @@ def greyfill():
             else:
                 img[i, j] = 1
 
-    print(img)
-    cv2.imshow("binary img", img)
+    #apply gaussian blur to binarized image
+    #rectangular kernel where height < height of vertical gap between text lines and width larger
+    #img_avg = cv2.GaussianBlur(img, (101, 3), 0, 0)
+
+    #img = np.empty((img_avg.shape[0], img_avg.shape[1]))
+    #threshold_2 = 0.9
+    #binarize img_avg to get initial estimate of text lines
+    #for i in range(0, img_avg.shape[0]):
+    #    for j in range(0, img_avg.shape[1]):
+    #        #print(blurred_img[i, j])
+    #        if(img_avg[i, j] < threshold_2):
+    #            img[i, j] = 0
+    #        else:
+    #            img[i, j] = 1
+
     #start filling operation:
     fv = 0.7
-    grayfill_img = np.zeros((img.shape[0], img.shape[1]))
+    #grayfill_img = np.zeros((img.shape[0], img.shape[1]))
     s = 0
     e = 0
     u = 0
     r = 8
     r_width = img.shape[1]//r
-    print(r_width)
-    print(img.shape)
     counter = 0
     for m1 in range(0, img.shape[0]):
         counter = 0
@@ -64,26 +73,17 @@ def greyfill():
         for j in range(1, img.shape[1]-1):
             if img[i, j] != fv and img[i+1, j] == fv  and img[i-1, j] == fv and img[i, j+1] == fv and img[i, j-1] == fv:
                 img[i, j] = fv
-    img[100, 100] = 0
-    #for m1 in range(0, img.shape[0]):
-    #    counter = 0
-    #    while(counter < r):
-            #print(counter)
-    #        for m2 in range(counter*r_width, (counter+1)*r_width):
-    #            img[m1, m2] = 0.2
-    #        counter += 1
-    print(img.shape)
-    #cv2.imshow("filled img", img)
-    #cv2.waitKey(0)
+    cv2.imshow("fill_igm", img)
+    cv2.waitKey(0)
     return img
 
 
-def detect_line_start():
+def detect_line_start(img):
     #read image in as greyscale
-    img = cv2.imread('line_sample.jpg', cv2.IMREAD_GRAYSCALE)
+    #img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     img = img/255
 
-    img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+    #img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
 
     threshold = 0.5
     #create binarized image to enhance lines
@@ -97,7 +97,6 @@ def detect_line_start():
 
     #apply gaussian blur to binarized image
     #rectangular kernel where height < height of vertical gap between text lines and width larger
-
     img_avg = cv2.GaussianBlur(img, (101, 3), 0, 0)
 
     img_initial = np.empty((img_avg.shape[0], img_avg.shape[1]))
@@ -111,6 +110,7 @@ def detect_line_start():
             else:
                 img_initial[i, j] = 1
 
+    #inverts initial_estimate for cc analysis
     for i in range(0, img_avg.shape[0]):
         for j in range(0, img_avg.shape[1]):
             #print(blurred_img[i, j])
@@ -119,21 +119,23 @@ def detect_line_start():
             else:
                 img_initial[i, j] = 0
 
-    img_initial = img_initial[0:800, :]
+    #img_initial = img_initial[0:800, :]
     img_initial = img_initial.astype(np.uint8)
 
     analysis = cv2.connectedComponentsWithStats(img_initial, 4, cv2.CV_32S) 
     (num_labels, label_ids, values, centroid) = analysis
-    print(f"Total labels: {num_labels}") 
-    print(f"label id: {label_ids.shape}")
-    print(f"CC values: {values}")
+   #print(f"heights: {heights}")
+    filtered_heights = np.empty((0))
+    for i in range(1, values.shape[0]):
+        if values[i, 4] > 500: #checks area
+            filtered_heights = np.append(filtered_heights, [values[i, 3]])
 
-    heights = values[1:, 3]
-    cc_med = int(np.median(heights.ravel()))
-    cc_avg = int(np.average(heights.ravel()))
-    print(f"median height: {cc_med}")
-    print(f"average height: {cc_avg}")
-    print(f"cc heights: {heights}")
+    #print(f"filtered_heights : {filtered_heights}")
+    cc_med = int(np.median(filtered_heights))
+    cc_avg = int(np.average(filtered_heights))
+    #print(f"median height: {cc_med}")
+    #print(f"average height: {cc_avg}")
+    #print(f"cc heights: {heights}")
     num_comps = 0
     output = np.zeros(img_initial.shape, dtype="uint8")
     for i in range(1, num_labels): 
@@ -141,10 +143,6 @@ def detect_line_start():
     
         if (area > 500): 
             num_comps += 1
-            # Labels stores all the IDs of the components on the each pixel 
-            # It has the same dimension as the threshold 
-            # So we'll check the component 
-            # then convert it to 255 value to mark it white 
             componentMask = (label_ids == i).astype("uint8") * 255
             
             # Creating the Final output mask 
@@ -158,9 +156,8 @@ def detect_line_start():
         if(counter > cc_avg):
             total_pixels = cc_med*img_initial.shape[1]
             num_pixels = np.count_nonzero(output[h:h+cc_med, :] == 255)
-            if(num_pixels > total_pixels*0.05):
+            if(num_pixels > total_pixels*0.075):
                 coordinates = np.append(coordinates, [h + cc_med/2])
-                print("found line")
                 num_lines_found += 1
                 counter = 0
         counter += 1
@@ -172,48 +169,32 @@ def detect_line_start():
                 output[i, j] = 255
             else:
                 output[i, j] = 0
-
-    for i in range(0, coordinates.shape[0]):
-        #print(coordinates[i])
-        cv2.circle(output, (0, int(coordinates[i])), 5, (0,0,255))
-    print(f"number of lines detected: {num_lines_found}")
-    print(f"reamining components: {num_comps}")
-    print(coordinates)
-    #cv2.circle(output, (300, 300), 5, (0,0,255))
-    #cv2.imshow('img_avg', img_avg)
-    #cv2.imshow('output', output)
-    #cv2.imshow('img_initial', 255*img_initial)
-    #cv2.waitKey(0)
     return coordinates.astype(int)
 
-
-
-
-
 def line_separator(greyfill_img, coordinates): 
+    line_separator = np.empty((coordinates.shape[0]), dtype=object)
+    count = 0
     for c in coordinates:
         print(f"coordinate: {c}")
         curr_w = 0
         curr_h = c
-        current_path = np.empty((0))
-        current_path = np.append(current_path, [c, 0], axis=0)
+        current_path = []
+        current_path.append([c, 0])
         flag = 'move_right_and_up'
-        move_up_fail = False
-        check_point_w = 0
         up = False
         down = False
         failed_up = False
         failed_down = False
         #move right
-        while curr_w < greyfill_img.shape[1]-2:
-            #print(f"current_h, current_w: {curr_h} {curr_w}")
+        while curr_w < greyfill_img.shape[1]-10 and curr_h < greyfill_img.shape[0]-1:
+            #Eprint(f"current_h, current_w: {curr_h} {curr_w}")
             #print(f"flag: {flag}")
             if flag == 'move_right_and_up' and failed_up == False:
                 #first always try to move right
                 if(greyfill_img[curr_h, curr_w+1]) == 0.7 and up == False: 
                     failed_down = False
                     curr_w += 1
-                    current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                    current_path.append([curr_h, curr_w])
                 else:
                     up = True
                 #then try to move down
@@ -221,52 +202,59 @@ def line_separator(greyfill_img, coordinates):
                     if greyfill_img[curr_h-1, curr_w] == 0.7:
                         up = False
                         curr_h -= 1
-                        current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        current_path.append([curr_h, curr_w])
                     elif greyfill_img[curr_h-1, curr_w-1] == 0.7:
                         up = False
                         curr_h -= 1
                         curr_w -= 1
-                        current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        current_path.append([curr_h, curr_w])
+                    elif greyfill_img[curr_h-1, curr_w+1] == 0 and greyfill_img[curr_h-1, curr_w] == 1:
+                        failed_up = True
+                        flag = 'move_right_and_down'
+                    elif greyfill_img[curr_h-1, curr_w] == 0 and greyfill_img[curr_h-1, curr_w-1] == 1:
+                        failed_up = True
+                        flag = 'move_right_and_down'
                     else:
                         curr_w -= 1
-                if greyfill_img[curr_h-1, curr_w+1] == 0 and greyfill_img[curr_h-1, curr_w] != 0.7: #detected blocked region
-                        failed_up = True
-                        #input("Press Enter to continue...")
-                        flag = 'move_right_and_down'
+                        current_path.append([curr_h, curr_w])
             if flag == 'move_right_and_down' and failed_down == False:
                 #first always try to move right
+                #print(down)
                 if(greyfill_img[curr_h, curr_w+1]) == 0.7 and down == False: 
                     failed_up = False
                     curr_w += 1
-                    current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                    current_path.append([curr_h, curr_w])
                 else:
                 #first check immediate down, then diagonal, then backtrack
                     down = True
                     if greyfill_img[curr_h+1, curr_w] == 0.7:
                         down = False
                         curr_h += 1
-                        current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        #current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        current_path.append([curr_h, curr_w])
                     elif greyfill_img[curr_h+1, curr_w-1] == 0.7:
                         down = False
                         curr_h += 1
                         curr_w -= 1
-                        current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        #current_path = np.append(current_path, [curr_h, curr_w], axis=0)
+                        current_path.append([curr_h, curr_w])
                     elif greyfill_img[curr_h+1, curr_w+1] == 0 and greyfill_img[curr_h+1, curr_w] == 1:
                         failed_down = True
                         flag = 'move_right_and_up'
-                        print(f"current flag: {flag}")
-                        print(f"current_h, current_w: {curr_h} {curr_w}")
+                       # print(f"current flag: {flag}")
+                       # print(f"current_h, current_w: {curr_h} {curr_w}")
                         #print(f"current move up check: {move_up_fail}")
-                        input("Press Enter to continue...")
+                       # input("Press Enter to continue...")
                     elif greyfill_img[curr_h+1, curr_w] == 0 and greyfill_img[curr_h+1, curr_w-1] == 1:
                         failed_down = True
                         flag = 'move_right_and_up'
-                        print(f"current flag: {flag}")
-                        print(f"current_h, current_w: {curr_h} {curr_w}")
+                       # print(f"current flag: {flag}")
+                       # print(f"current_h, current_w: {curr_h} {curr_w}")
                         #print(f"current move up check: {move_up_fail}")
-                        input("Press Enter to continue...")
+                       # input("Press Enter to continue...")
                     else:
                         curr_w -= 1
+                        current_path.append([curr_h, curr_w])
 
                 #if greyfill_img[curr_h+1, curr_w+1] == 0 and greyfill_img[curr_h+1, curr_w] != 0.7: #detected blocked region
                 #        print(f"current flag: {flag}")
@@ -276,27 +264,98 @@ def line_separator(greyfill_img, coordinates):
                 #        failed_down = True
                 #        flag = 'move_right_and_up'
             if failed_up and failed_down:
-                print(f"current flag: {flag}")
-                print(f"current_h, current_w: {curr_h} {curr_w}")
+                #print(f"current flag: {flag}")
+                #print(f"current_h, current_w: {curr_h} {curr_w}")
                 #print(f"current move up check: {move_up_fail}")
-                input("Press Enter to continue...")
+                #input("Press Enter to continue...")
                 pass
+        line_separator[count] = np.array(current_path)
+        count += 1
+    return line_separator
 
+def output_lines(lines, img):
+    #print(img.shape)
+    old_start = 0
+    counter = 0
+    line_count = 0
+    for i in range(0, lines.shape[0]):
+        path = lines[i]
+        #find min/max heights 
+        min = np.min(path, axis=0)
+        max = np.max(path, axis=0)
+        new_start = max[0]
+        #print(f"min for {i}: {min}")
+        #print(f"max for {i}: {max}")
+        height = max[0] - min[0]
+        width = max[1] - min[1]
+        #print("current max_height", height)
+        line_img = np.ones((new_start - old_start, width))
+        #print(line_img.shape)
+        #print(line_img.shape)
+        for h in range(0, line_img.shape[0]):
+            for c in range(0, line_img.shape[1]):
+                #print(img[old_start+h, c, :])
+                line_img[h, c] = img[old_start+h, c]
+        #print(line_img)
+        #tot_pixels = 0
+        #black_pixels = 0
+        old_start = min[0]
+        counter += 1
+        if(line_img.shape[0]>0):
+            tot_pixels = 0
+            black_pixels = 0
+            for i in range(0, line_img.shape[0]):
+                for j in range(0, line_img.shape[1]):
+                    tot_pixels += 1
+                    if line_img[i, j] != 1:
+                        black_pixels += 1
+            #print(black_pixels/tot_pixels)
 
-                
-greyfill_img = greyfill()
-coordinates = detect_line_start()
-line_separator(greyfill_img, coordinates)
+            #cv2.imshow('generated_img', line_img/255)
+            #cv2.waitKey(0)
+            name = "text_line" + str(line_count) + ".jpg"
+            #os.chdir('\text_lines')
+            #cwd = os.getcwd()
+    
+            # print the current directory
+            #print("current directory:" , cwd)
+            #path = cwd + '/text_lines'
+            #os.chdir(path)
+            line_count += 1
+            cv2.imwrite(name, line_img)
+            #cv2.waitKey(0)
 
+img = cv2.imread('output_clean_p2.jpg', cv2.IMREAD_GRAYSCALE)
+img = img[50:img.shape[0]-50, 50:img.shape[1]-50]
+cv2.imshow("cropped_img", img)
+cv2.waitKey(0)
+#img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+
+greyfill_img = greyfill(img)
+#cv2.imshow("greyfill_img", img)
+coordinates = detect_line_start(img)
+#print(coordinates)
+lines = line_separator(greyfill_img, coordinates)
+
+output_lines(lines, img)
+for i in range(0, lines.shape[0]):
+    path = lines[i]
+    for j in range(0, path.shape[0]):
+            point = path[j, :]
+            img[point[0], point[1]] = 0
+
+cv2.imshow('final_img', img)
 for i in range(0, coordinates.shape[0]):
     #print(coordinates[i])
     cv2.circle(greyfill_img, (0, coordinates[i]), 5, (0,0,255))
 
-ph = 67
-pw = 125
+ph = 110
+pw = 10
 greyfill_img[ph, pw] = 0.3
-print(greyfill_img[ph-7:ph+7, pw-7:pw+7])
+#rint(greyfill_img[ph-7:ph+7, pw-7:pw+7])
 #cv2.circle(greyfill_img, (623, 25), 2, (0, 0,255))
 cv2.imshow("part", greyfill_img[ph-10:ph+10, pw-10:pw+10])
 cv2.imshow("greyfill_img", greyfill_img)
 cv2.waitKey(0)
+
+#line_separator(greyfill_img, coordinates)
